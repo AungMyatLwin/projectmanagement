e068b01fc096934eba221aaedfad8e0900e46431
AungMyatLwin <zack.cloud7@gmail.com>
2020-12-19 15:26:43 +0000

    models update

--- Pmanage/models.py
+++ Pmanage/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    Project_name=models.CharField(max_length=64)
+    tasks=models.IntegerField()
+    team=models.IntegerField()
+    def __str__():
+        return f'{Project_name}'
--- Pmanage/views.py
+++ Pmanage/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render,HttpResponse
-
+from .models import Project
 # Create your views here.
 def index(request):
-    return HttpResponse("<h1>Hello</h1>")
+    b=Project.objects.all()
+    for na in b:
+        name=na.Project_name
+    return HttpResponse(f'<h1>Hello{name}</h1>')
     
 def plan(request):
     return HttpResponse("<h1>Hello</h1>")
