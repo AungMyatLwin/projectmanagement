7ad2824bdb2c989cf90377d6505c499317e671f9
AungMyatLwin <zack.cloud7@gmail.com>
2020-12-22 00:20:39 +0630

    real login view implemented along with layout,login.html

--- Pmanage/views.py
+++ Pmanage/views.py
@@ -1,16 +1,26 @@
 from django.shortcuts import render,HttpResponse
-from django.contrib.auth import authenticate, login,logout as Lout
+from django.contrib.auth import authenticate, login as lagin,logout as Lout
+from django.contrib.auth.models import User
 from .models import Project
 # Create your views here.
 def login(request):
-    return render(request,"Pmanage/login.html")
+    if request.method == 'POST':
+        username=request.POST["Username"]
+        password=request.POST["Password"]
+        print(username,'and', password)
+        struser=str(username)
+        strpass=str(password)
+        user = authenticate(username=struser, password=strpass)
+        lagin(request,user)
+        return render(request,"Pmanage/index.html")
+    return HttpResponse("wer")
 
 def logout(request):
     Lout(request)
     return render(request,"Pmanage/logout.html")
 
 def index(request):
-    return render(request,"Pmanage/index.html")
+    return render(request,"Pmanage/login.html")
     
 def plan(request):
     return render(request,"Pmanage/Plan.html")
