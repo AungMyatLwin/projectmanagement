fe86ac3d3b290d8b81174b95273a8f31be6bbd3f
AungMyatLwin <zack.cloud7@gmail.com>
2020-12-22 17:21:41 +0630

    updates

--- Pmanage/views.py
+++ Pmanage/views.py
@@ -1,36 +1,68 @@
-from django.shortcuts import render,HttpResponse
-from django.contrib.auth import authenticate, login as lagin,logout as Lout
+from django.shortcuts import render, HttpResponse
+from django.contrib.auth import authenticate, login as lagin, logout as Lout
 from django.contrib.auth.models import User
-from .models import Project
-# Create your views here.
-def login(request):
-    if request.method == 'POST':
-        username=request.POST["Username"]
-        password=request.POST["Password"]
-        print(username,'and', password)
-        struser=str(username)
-        strpass=str(password)
-        user = authenticate(username=struser, password=strpass)
-        lagin(request,user)
-        return render(request,"Pmanage/index.html")
-    return HttpResponse("wer")
+from django.contrib.auth.decorators import login_required
+from .models import Project,User
 
-def logout(request):
-    Lout(request)
-    return render(request,"Pmanage/logout.html")
+
+# Create your views here.
 
 def index(request):
-    return render(request,"Pmanage/login.html")
-    
+    if request.user.is_authenticated:
+        return render(request, "Pmanage/index.html")
+    else:
+        return render(request, "Pmanage/login.html")
+
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST["Username"]
+        password = request.POST["Password"]
+        print(username, 'and', password)
+        struser = str(username)
+        strpass = str(password)
+        user = authenticate(request,username=struser, password=strpass)
+        if user is not None:
+            lagin(request, user)
+            return render(request, "Pmanage/index.html")
+        else:
+            return render(request, "Pmanage/login.html")
+    return HttpResponse("wer")
+
+@login_required()
+def logout(request):
+    Lout(request)
+    return render(request, "Pmanage/logout.html")
+
+
+
+@login_required
+def createproject(request):
+    return render(request, "Pmanage/Plan.html")
+
+@login_required
 def plan(request):
+    project_title=request.POST["Pname"]
+    team=request.POST["team"]
+    maxday=request.POST["maxday"]
+    print(project_title,"",team,"",maxday)
+    projects=Project.objects.all()
+    return render(request, "Pmanage/createplan.html",{
+        "projects":projects
+    })
+
+@login_required
+def create_plan(request):
     return render(request,"Pmanage/Plan.html")
 
+
 def track(request):
-    return render(request,"Pmanage/Track.html")
+    return render(request, "Pmanage/Track.html")
 
 
 def release(request):
-    return render(request,"Pmanage/Release.html")
+    return render(request, "Pmanage/Release.html")
+
 
 def report(request):
-    return render(request,"Pmanage/Report.html")
+    return render(request, "Pmanage/Report.html")
