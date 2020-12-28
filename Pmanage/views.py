05b7121c16f22335af67f3a2abdcf0498685f1da
AungMyatLwin <zack.cloud7@gmail.com>
2020-12-28 18:48:33 +0630

    fixed

--- Pmanage/views.py
+++ Pmanage/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, HttpResponse, HttpResponseRedirect, reverse
 from django.contrib.auth import authenticate, login as lagin, logout as Lout
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
-from .models import Project, User, Tasks
+from .models import Project, User, Tasks,Report
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 import json
@@ -153,6 +153,6 @@ def report(request):
 
 def reported(request,id):
     text=request.POST["Report"]
-    reports=report.objects.create(proid_id=id,Report=text)
+    reports=Report.objects.create(proid_id=id,Reports=text)
     reports.save()
     return HttpResponse(f'{id} and {text}')
